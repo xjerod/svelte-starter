563814f54f3ac30f381b75e24a514384419a970d
Jerod <jerod@synth.sh>
2023-05-29 08:54:22 -0700

    update template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="flex flex-col items-center justify-center w-full h-screen">
+	<div class="flex">Sup &trade;</div>
+</div>
